44b71b4daba2cb18d6077d37d3ea0b239381f300
nursh <nsheikhmohammed@yahoo.com>
2018-08-16 14:57:14 -0400

    Added a second template for app

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,53 @@
 'use strict';
 
 var template = React.createElement(
-  'p',
+  'div',
   null,
-  'This is a JSX render'
+  React.createElement(
+    'h1',
+    null,
+    'Indecision App'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Something Something'
+  ),
+  React.createElement(
+    'ol',
+    null,
+    React.createElement(
+      'li',
+      null,
+      'First item'
+    ),
+    React.createElement(
+      'li',
+      null,
+      'Second item'
+    )
+  )
+);
+
+var templateTwo = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    'Nuradeen Sheikh'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Age: 23'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Location: Ottawa, Canada'
+  )
 );
 var app = document.getElementById('app');
 
-ReactDOM.render(template, app);
+ReactDOM.render(templateTwo, app);
--- src/app.js
+++ src/app.js
@@ -1,4 +1,21 @@
-const template = <p>This is a JSX render</p>;
+const template = (
+  <div>
+    <h1>Indecision App</h1>
+    <p>Something Something</p>
+    <ol>
+      <li>First item</li>
+      <li>Second item</li>
+    </ol>
+  </div>
+);
+
+const templateTwo = (
+  <div>
+    <h1>Nuradeen Sheikh</h1>
+    <p>Age: 23</p>
+    <p>Location: Ottawa, Canada</p>
+  </div>
+);
 const app = document.getElementById('app');
 
-ReactDOM.render(template, app);
+ReactDOM.render(templateTwo, app);
