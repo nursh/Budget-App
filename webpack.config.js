4294cbe77c7d8dd7e43edb25670f9b918e70f254
nursh <nsheikhmohammed@yahoo.com>
2018-08-28 08:55:07 -0400

    Configured webpack-dev-server to serve project files

--- webpack.config.js
+++ webpack.config.js
@@ -6,9 +6,15 @@ module.exports = {
     path: path.join(__dirname, 'public'),
     filename: 'bundle.js',
   },
+  devtool: 'cheap-module-eval-source-map',
   module: {
     rules: [
       { test: /\.js$/, exclude: /node_modules/, use: 'babel-loader' }
     ],
   },
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    hot: true,
+    inline: false,
+  }
 }
